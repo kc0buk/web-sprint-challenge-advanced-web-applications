06d70558b4478d2dbddf7506795c1f4ba463a38a
Jeremiah Trnka <jeremiah@jeremiahs-mbp.lan>
2020-08-21 09:40:16 -0700

    Added components to enable app login and redirect to private route bubbles.

---
+++ client/src/hooks/useAPI.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import { axiosWithAuth } from '../utils/axiosWithAuth'
+
+export const useAPI = (config) => {
+    const [dataAPI, setData] = useState('')
+    const [error, setError] = useState('')
+    const { method, url, data } = config
+
+    const moveData = () => {
+         return axiosWithAuth()
+            [method](url, data)
+            .then( res => res.data)
+            .catch( err => setError(err))
+
+    }
+    // console.log(dataAPI)
+    return [dataAPI, moveData, error]
+}
